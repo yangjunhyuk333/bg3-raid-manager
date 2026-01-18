dc964ae0376e9881ab58ce04988fad06de0a98ea
yangjunhyuk333 <yangjunhyuk333@users.noreply.github.com>
2026-01-18 22:33:09 +0900

    Enable PWA Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
